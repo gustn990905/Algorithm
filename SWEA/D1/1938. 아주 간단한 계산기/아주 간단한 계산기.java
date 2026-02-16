77ab289327168700aafa866f46797e768fab0e0d
gustn990905 <julie019019@gmail.com>
2026-02-16 15:51:13 +0900

    [D1] Title: 아주 간단한 계산기, Time: 96 ms, Memory: 26,752 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println(a + b); // 덧셈
+		System.out.println(a - b); // 뺄셈
+		System.out.println(a * b); // 곱셈
+		System.out.println(a / b); // 정수 나눗셈
+	}
+}
