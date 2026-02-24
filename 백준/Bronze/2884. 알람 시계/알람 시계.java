f7e97e33aa14a714b1dd1e23ba7cd7247b4e0937
gustn990905 <julie019019@gmail.com>
2026-02-24 21:41:59 +0900

    [Bronze III] Title: 알람 시계, Time: 196 ms, Memory: 18536 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+        
+        M -= 45;
+        
+        if(M>=0 && M<60) {
+        	System.out.println(H + " " + M);
+        } else if(M<0) {
+        	M += 60;
+        	H -= 1;
+        	if(H>=0 && H<24) {
+        		System.out.println(H + " " + M);
+        	} else {
+        		H += 24;
+        		System.out.println(H + " " + M);
+        	}
+        }
+    }
+}
