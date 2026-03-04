bc209a4c9695daba7d4b022e0ae9e111de4eb1a3
gustn990905 <julie019019@gmail.com>
2026-03-04 11:23:42 +0900

    [D2] Title: 쉬운 거스름돈, Time: 107 ms, Memory: 27,904 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -5,21 +5,22 @@ public class Solution {
 		Scanner sc = new Scanner(System.in);
 
 		int T = sc.nextInt();
-		// 화폐 단위 배열 (큰 단위 -> 작은 단위 순서)
-		int[] money = { 50000, 10000, 5000, 1000, 500, 100, 50, 10 };
-		for (int t = 1; t <= T; t++) {
-			System.out.println("#" + t);
+		
+		int[] money = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+		int[] moneyCheck = new int[8];
 
+		for (int t = 1; t <= T; t++) {
 			int N = sc.nextInt();
 			
-			/*
-			 * 각 화폐 단위별 개수 계산
-			 * 큰 단위부터 차례대로 나눠서 개수 구하기
-			 */
+			for(int m=0; m<8; m++) {
+				moneyCheck[m] = N / money[m];
+				
+				N = N % money[m];
+			}
 			
-			for(int i=0; i<8; i++) {
-				System.out.print(N/money[i] + " ");
-				N = N % money[i];
+			System.out.println("#" + t);
+			for(int m=0; m<8; m++) {
+				System.out.print(moneyCheck[m] + " ");
 			}
 			System.out.println();
 		}
