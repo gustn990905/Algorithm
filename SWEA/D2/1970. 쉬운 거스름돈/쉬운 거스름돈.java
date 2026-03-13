cbf096724bc5368e7b6e4c7f2377153f1d42fe05
gustn990905 <julie019019@gmail.com>
2026-03-13 13:16:56 +0900

    [D2] Title: 쉬운 거스름돈, Time: 107 ms, Memory: 27,904 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -1,26 +1,48 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		int T = sc.nextInt();
 		
-		int[] money = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
-		int[] moneyCheck = new int[8];
-
-		for (int t = 1; t <= T; t++) {
-			int N = sc.nextInt();
+		int[] coins = {5000, 1000, 500, 100, 50, 10, 5, 1};
+		
+		int T = sc.nextInt();
+		for(int t=1; t<=T; t++) {
+			int money = sc.nextInt() / 10;
+			int[] dp = new int[money+1];
+			int[] last = new int[money+1];
+			 
+			Arrays.fill(dp, Integer.MAX_VALUE);
+			dp[0] = 0;
 			
-			for(int m=0; m<8; m++) {
-				moneyCheck[m] = N / money[m];
+			for(int i=1; i<=money; i++) {				
+				for(int c : coins) {
+					if(i-c>=0 && dp[i-c] != Integer.MAX_VALUE) {
+						if(dp[i] > dp[i-c] + 1) {
+							dp[i] = dp[i-c] + 1;
+							last[i] = c;
+						}
+					}
+				}
+			}
+			
+			int[] count = new int[coins.length];
+			int m = money;
+			while(m>0) {
+				int c = last[m];
+				for(int i=0; i<coins.length; i++) {
+					if(coins[i] == c) {
+						count[i]++;
+					}
+				}
 				
-				N = N % money[m];
+				m -= c;
 			}
 			
 			System.out.println("#" + t);
-			for(int m=0; m<8; m++) {
-				System.out.print(moneyCheck[m] + " ");
+			for(int i=0; i<coins.length; i++) {
+				System.out.print(count[i] + " ");
 			}
 			System.out.println();
 		}
