85c3a4a044163cae7cf383b589eb12d653561b2d
gustn990905 <julie019019@gmail.com>
2026-03-23 21:22:10 +0900

    [Bronze III] Title: 세 막대, Time: 176 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[3];
+		
+		for(int i=0; i<3; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		while(arr[0] + arr[1] <= arr[2]) {
+			arr[2]--;
+		}
+		
+		int result = 0;
+		for(int i=0; i<3; i++) {
+			result+=arr[i];
+		}
+		
+		System.out.println(result);
+	}
+}
