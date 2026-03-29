0d1676578ce12c1a920b2deac0b23d7ffb4d5025
gustn990905 <julie019019@gmail.com>
2026-03-29 21:07:19 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 592 ms, Memory: 49820 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][2];
+		
+		for(int n=0; n<N; n++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[n][0] = Integer.parseInt(st.nextToken());
+			arr[n][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr, (a, b) -> {
+			if(a[0] == b[0]) {
+				return a[1] - b[1];
+			}
+			return a[0] - b[0];
+		});
+		
+		StringBuilder sb = new StringBuilder();
+		for(int n=0; n<N; n++) {
+			sb.append(arr[n][0]).append(" ").append(arr[n][1]).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
