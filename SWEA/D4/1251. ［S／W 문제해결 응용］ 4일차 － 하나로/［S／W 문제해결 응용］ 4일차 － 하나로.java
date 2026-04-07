c411e5f13dc08fe3c8b44306e83a5766c1c38d25
gustn990905 <julie019019@gmail.com>
2026-04-07 12:14:31 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 599 ms, Memory: 99,584 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,119 +1,93 @@
+import java.awt.Adjustable;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
 
-	static int N;
-	static List<Edge> edgeList;
-	static int[] parent;
-
-	static class Edge {
-		int from, to;
-		long cost;
-
-		Edge(int from, int to, long cost) {
-			this.from = from;
-			this.to = to;
-			this.cost = cost;
-		}
-	}
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		
 		int T = Integer.parseInt(br.readLine());
-
-		for (int t = 1; t <= T; t++) {
-			N = Integer.parseInt(br.readLine()); // 섬의 개수
-
-			StringTokenizer x = new StringTokenizer(br.readLine()); // 섬들의 정수인 X 좌표
-			int[] X = new int[N];
-			for (int i = 0; i < N; i++) {
-				X[i] = Integer.parseInt(x.nextToken());
+		
+		for(int t=1; t<=T; t++) {
+			int N = Integer.parseInt(br.readLine()); // 섬의 개수
+			
+			StringTokenizer X = new StringTokenizer(br.readLine()); // 섬들의 x 좌표
+			int[] x = new int[N];
+			for(int i=0; i<N; i++) {
+				x[i] = Integer.parseInt(X.nextToken());
 			}
-
-			StringTokenizer y = new StringTokenizer(br.readLine()); // 섬들의 정수인 Y 좌표
-			int[] Y = new int[N];
-			for (int i = 0; i < N; i++) {
-				Y[i] = Integer.parseInt(y.nextToken());
+			
+			StringTokenizer Y = new StringTokenizer(br.readLine()); // 섬들의 y 좌표
+			int[] y = new int[N];
+			for(int i=0; i<N; i++) {
+				y[i] = Integer.parseInt(Y.nextToken());
 			}
-
-			double E = Double.parseDouble(br.readLine()); // 환경 부담 세율
-
-			parent = new int[N]; // 섬의 시작점을 0으로 둘 것
-			for (int i = 0; i < N; i++) {
-				parent[i] = i;
-			}
-
-			edgeList = new ArrayList<>();
-
+			
+			double E = Double.parseDouble(br.readLine()); // 세율
+			
+			long[][] adjList = new long[N][N];
+			
 			// 모든 간선 만들기
-			for (int i = 0; i < N - 1; i++) {
-				for (int j = i + 1; j < N; j++) {
-					long dx = X[i] - X[j];
-					long dy = Y[i] - Y[j];
-
-					long dist = dx * dx + dy * dy;
-					edgeList.add(new Edge(i, j, dist));
+			// i는 from
+			// j는 to
+			for(int i=0; i<N-1; i++) {
+				for(int j=i+1; j<N; j++) {
+					long dx = x[i] - x[j];
+					long dy = y[i] - y[j];
+					long cost = dx*dx + dy*dy;
+					
+					adjList[i][j] = cost;
+					adjList[j][i] = cost;
 				}
 			}
-
-			// cost가 짧은 순서로 정렬
-			Collections.sort(edgeList, (a, b) -> {
-				return Long.compare(a.cost, b.cost);
-			});
-
-			long result = kruscal();
-			System.out.printf("#%d %.0f \n", t, result * E);
-		}
-	}
-
-	static long kruscal() {
-		long sum = 0; // cost의 합
-
-		int cnt = 0; // 합친 섬의 개수
-
-		for (Edge edge : edgeList) {
-			int v1 = edge.from;
-			int v2 = edge.to;
-			long cost = edge.cost;
-
-			// 부모 노드가 다른 경우에만 합치기
-			if (union(v1, v2)) {
-				sum += cost;
-				cnt++;
+			
+			// 가중치(거리 정보)
+			long[] dist = new long[N];
+			// 방문 체크
+			boolean[] visited = new boolean[N];
+			
+			// dist 배열 초기화
+			for(int n=0; n<N; n++) {
+				dist[n] = Long.MAX_VALUE;
 			}
+			
+			dist[0] = 0; // 시작점을 0으로 고름
+			long ans = 0;
+			
+			for(int i=0; i<N-1; i++) {
+				// 방문을 하지 않았으면서 dist가 가장 작은 값을 찾기
+				long min = Long.MAX_VALUE;
+				int idx = -1;
+				
+				for(int j=0; j<N; j++) {
+					if(!visited[j] && dist[j] < min) {
+						min = dist[j];
+						idx = j;
+					}
+				}
+				
+				
+				visited[idx] = true;
+				
+				// 방문한 섬과 연결되어 있는 점들 중 방문하지 않은 섬 찾기
+				// 방문하지 않은 섬들 중 더 짧은 거리 저장
+				for(int j=0; j<N; j++) {
+					if(!visited[j] && adjList[idx][j] != 0 && dist[j] > adjList[idx][j]) {
+						dist[j] = adjList[idx][j];
+					}
+				}
 
-			if (cnt >= N - 1)
-				break;
+			}
+			for(int i=0; i<N; i++) {
+				ans += dist[i];
+			}
+			
+			System.out.printf("#%d %.0f\n", t, ans*E);
 		}
-
-		return sum;
-	}
-
-	static boolean union(int v1, int v2) {
-		int v1Root = find(v1);
-		int v2Root = find(v2);
-
-		// 부모 노드가 같으면 false 반환
-		if (v1Root == v2Root)
-			return false;
-
-		// 부모 노드가 다르면 합치기
-		parent[v2Root] = v1Root;
-		return true;
-	}
-
-	static int find(int v) {
-		if (parent[v] == v)
-			return v;
-
-		return parent[v] = find(parent[v]);
 	}
 }
