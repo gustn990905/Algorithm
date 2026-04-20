45d4c39b25ff059c75d193cf8f49cb72a46bc8f8
gustn990905 <julie019019@gmail.com>
2026-04-20 09:01:21 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 120 ms, Memory: 30,444 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Solution {
 
-	static int dayCost;
-	static int monthCost;
+	static int oneDayCost;
+	static int oneMonthCost;
 	static int threeMonthCost;
 	static int yearCost;
 
@@ -13,30 +13,38 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int T = sc.nextInt();
+		int T = sc.nextInt(); // 테스트 케이스 수
+
 		for (int t = 1; t <= T; t++) {
-			dayCost = sc.nextInt();
-			monthCost = sc.nextInt();
+			oneDayCost = sc.nextInt();
+			oneMonthCost = sc.nextInt();
 			threeMonthCost = sc.nextInt();
 			yearCost = sc.nextInt();
 
 			plan = new int[12];
-			for (int m = 0; m < 12; m++) {
-				plan[m] = sc.nextInt();
+
+			// 수영장 이용 계획
+			for (int i = 0; i < 12; i++) {
+				plan[i] = sc.nextInt();
 			}
 
-			finalCost = yearCost;
-			totalCost(0, 0);
+			finalCost = yearCost; // 일년 비용으로 초기화
+
+			findMinCost(0, 0);
+
 			System.out.println("#" + t + " " + finalCost);
 		}
 	}
 
-	public static void totalCost(int month, int money) {
+	static void findMinCost(int month, int money) {
+
+		// 12월 이상 다 확인하면 최대 비용으로 갱신
 		if (month >= 12) {
-			if (finalCost > money) {
+			if (money < finalCost) {
 				finalCost = money;
 				return;
-			} return;
+			}
+			return;
 		}
 
 		oneDay(month, money);
@@ -44,15 +52,18 @@ public class Solution {
 		threeMonth(month, money);
 	}
 
-	public static void oneDay(int month, int money) {
-		totalCost(month+1, money + plan[month]*dayCost);
+	static void oneDay(int month, int money) {
+		findMinCost(month + 1, money + plan[month] * oneDayCost);
 	}
 
-	public static void oneMonth(int month, int money) {
-		totalCost(month+1, money + monthCost);
+	static void oneMonth(int month, int money) {
+		if (plan[month] == 0)
+			findMinCost(month + 1, money);
+		else
+			findMinCost(month + 1, money + oneMonthCost);
 	}
 
-	public static void threeMonth(int month, int money) {
-		totalCost(month+3, money + threeMonthCost);
+	static void threeMonth(int month, int money) {
+		findMinCost(month + 3, money + threeMonthCost);
 	}
 }
