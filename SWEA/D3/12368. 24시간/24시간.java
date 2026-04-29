3f9a305f195aa97ab69a2a316b78bda27cd5cacf
gustn990905 <julie019019@gmail.com>
2026-04-29 16:31:22 +0900

    [D3] Title: 24시간, Time: 130 ms, Memory: 31,864 KB -BaekjoonHub

---
+++ SWEA/D3/12368. 24시간/24시간.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt(); 
+		
+		for(int t=1; t<=T; t++) {
+			
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			System.out.println("#" + t + " " + ((A+B) % 24));
+		}
+	}
+}
