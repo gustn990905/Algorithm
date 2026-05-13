f250e3c490caffe94f3eea49f67db23fdc51c082
gustn990905 <julie019019@gmail.com>
2026-05-13 14:49:26 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 1,324 ms, Memory: 204,960 KB -BaekjoonHub

--- SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
@@ -1,44 +1,48 @@
 import java.util.Scanner;
 
 public class Solution {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		int T = sc.nextInt();
 		
 		for(int t=1; t<=T; t++) {
-			int N = sc.nextInt();
+			int N = sc.nextInt(); // 정수 N
 			
 			int[] arr = new int[N];
+			
 			for(int n=0; n<N; n++) {
 				arr[n] = sc.nextInt();
 			}
 			
-			int result = -1;
+			// 1. 숫자 2개를 뽑아서 곱하기
+			// 2. 곱한 숫자가 단조 증가하는 수인지 확인
+			// 3. 단조 증가하는 수 중에서 가장 큰 값으로 갱신
 			
-			// 숫자 2개 뽑기
-			for(int i=0; i<N-1; i++) {
-				for(int j=i+1; j<N; j++) {
+			int result = 0;
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					// 같은 수를 뽑은 경우는 존재하면 안됨
+					if(i==j) continue;
 					
-					// 숫자를 하나씩 확인하기 위해서 String으로 변환
-					String num = String.valueOf(arr[i]*arr[j]);
+					int multi = arr[i] * arr[j];
 					
-					boolean possible = true;
-					for(int k=0; k<num.length()-1; k++) {
-						
-						// 다음수가 그 전보다 수 적으면 possible=false로 변경 
-						if(num.charAt(k) > num.charAt(k+1)) {
-							possible = false;
-							break;
-						}
-					}
-					
-					if(possible) result = Math.max(result, arr[i]*arr[j]);
+					// 단조 증가하는 수이면 가장 큰 값으로 result 값 갱신
+					if(check(multi)) result = Math.max(result, multi);
 				}
 			}
 			
-			System.out.println("#" + t + " " + result);
+			if(result == 0) System.out.println("#" + t + " -1");
+			else System.out.println("#" + t + " " + result);
 		}
+	}
+	
+	static boolean check(int multi) {
+		String num = String.valueOf(multi);
+		
+		for(int i=0; i<num.length()-1; i++) {
+			if(num.charAt(i)-'0' > num.charAt(i+1)-'0') return false;
+		} 
+		return true;
 	}
 }
