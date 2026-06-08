87f0c9059b05c90c72dc2da05d64356f8a2d3514
gustn990905 <julie019019@gmail.com>
2026-06-08 15:12:58 +0900

    [D3] Title: 오목 판정, Time: 78 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.java
+++ SWEA/D3/11315. 오목 판정/오목 판정.java
@@ -3,49 +3,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution {
-	public static void main(String[] args) throws IOException {
+class Solution {
+	
+	// 오 하 하좌 하우
+	static int[] dr = {0, 1, 1, 1};
+	static int[] dc = {1, 0, -1, 1};
+	
+    public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		
 		int T = Integer.parseInt(br.readLine());
 		
-		// 8방향 탐색을 위한 방향 배열 (상, 하, 좌, 우 + 대각선)
-		int[] dr = {-1, -1, -1, 0, 0, 1, 1, 1};
-		int[] dc = {-1, 0, 1, -1, 1, -1, 0, 1};
-
-		for (int t = 1; t <= T; t++) {
-			int N = Integer.parseInt(br.readLine().trim());
-			char[][] map = new char[N][N];
+		for(int t=1; t<=T; t++) {
+			int N = Integer.parseInt(br.readLine());
+			
+			char[][] arr = new char[N][N];
 			
 			for(int r=0; r<N; r++) {
-				String line = br.readLine().trim();
+				String line = br.readLine();
 				for(int c=0; c<N; c++) {
-					map[r][c] = line.charAt(c);
+					arr[r][c] = line.charAt(c);
 				}
 			}
 			
-			// 결과 저장 (5목 이상이면 갱신됨)
-			int result = 0;
-			loop: // 전체 탐색 중 조건 만족 시 바로 탈출하기 위한 라벨
+			boolean possible = false;
+			
+			loop:
 			for(int r=0; r<N; r++) {
 				for(int c=0; c<N; c++) {
-					if(map[r][c] == 'o') {
-						for(int i=0; i<8; i++) {
-							int cnt = 1; // 현재 위치 포함 1개부터 시작
+					if(arr[r][c] == 'o') {
+						for(int d=0; d<4; d++) {
+							int cnt = 1; // 현재 위치 포함
 							int time = 0;
+							
 							while(true) {
 								time++;
-								int nr = r + dr[i]*time;
-								int nc = c + dc[i]*time;
+								int nr = r + dr[d]*time;
+								int nc = c + dc[d]*time;
 								
-								if(nr<0 || nr>=N || nc<0 || nc>=N) break;
+								if(nr < 0 || nr >= N || nc < 0 || nc >= N) break;
 								
-								// 빈칸이면 더 이상 연속되지 않으므로 종료
-								if(map[nr][nc] == '.') break;
+								// 빈칸이 들어오면 더 이상 연속되지 않음
+								if(arr[nr][nc] == '.') break;
 								else cnt++;
 								
 								if(cnt >= 5) {
-									result = cnt;
+									possible = true;
 									break loop;
 								}
 							}
@@ -53,8 +56,7 @@ public class Solution {
 					}
 				}
 			}
-			
-			if(result >= 5) System.out.println("#" + t + " " + "YES");
+			if(possible) System.out.println("#" + t + " " + "YES");
 			else System.out.println("#" + t + " " + "NO");
 		}
 	}
