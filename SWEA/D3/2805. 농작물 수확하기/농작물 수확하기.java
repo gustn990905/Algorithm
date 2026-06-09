e381643e4371f88d6737b578c98d3fe3d9a7dbc9
gustn990905 <julie019019@gmail.com>
2026-06-09 13:29:22 +0900

    [D3] Title: 농작물 수확하기, Time: 145 ms, Memory: 32,616 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,44 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
-public class Solution {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class Solution {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		int T = Integer.parseInt(br.readLine());
-		
+		int T = sc.nextInt();
 		for(int t=1; t<=T; t++) {
-			int N = Integer.parseInt(br.readLine()); // 농장의 크기
+			int N = sc.nextInt();
 			
-			int[][] farm = new int[N][N];
-			
+			int[][] map = new int[N][N];
 			for(int r=0; r<N; r++) {
-				String line = br.readLine();
+				String line = sc.next();
 				for(int c=0; c<N; c++) {
-					farm[r][c] = line.charAt(c) - '0';
+					map[r][c] = line.charAt(c) - '0';
 				}
 			}
 			
-			int result = 0;
-			
-			// 농장의 위쪽의 농작물 합
-			for(int i=0; i<=N/2; i++) {
-				for(int j=N/2-i; j<=N/2+i; j++) {
-					result += farm[i][j];
+			int sum = 0;
+			for(int r=0; r<=N/2; r++) {
+				for(int c=N/2-r; c<=N/2+r; c++) {
+					sum += map[r][c];
 				}
 			}
 			
-			// 아래쪽 농작물 합
-			int size = N/2-1;
-			for(int i=N/2+1; i<N; i++) {
-				for(int j=N/2-size; j<=N/2+size; j++) {
-					result += farm[i][j];
+			for(int r=N/2+1; r<N; r++) {
+				for(int c=N/2-(N-1-r); c<=N/2+(N-1-r); c++) {
+					sum += map[r][c];
 				}
-				size--;
 			}
 			
-			System.out.println("#" + t + " " + result);
+			System.out.println("#" + t + " " + sum);
 		}
 	}
 }
