db5cc924bfe2a348fdebc6a51b347eda428869ed
gustn990905 <julie019019@gmail.com>
2026-06-11 23:43:16 +0900

    [D3] Title: 공과 잡초, Time: 119 ms, Memory: 28,160 KB -BaekjoonHub

--- SWEA/D3/14555. 공과 잡초/공과 잡초.java
+++ SWEA/D3/14555. 공과 잡초/공과 잡초.java
@@ -1,44 +1,36 @@
 import java.util.Scanner;
 
-public class Solution {
-	public static void main(String[] args) {
+class Solution {
+    public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+				
 		int T = sc.nextInt();
 		
 		for(int t=1; t<=T; t++) {
-			StringBuilder grass = new StringBuilder(); // 문자열을 저장할 StringBuilder 생성
-			
-			grass.append(sc.next());
+			String line = sc.next();
 			
 			int ball = 0;
-			int cnt = 0; // '|' (잡초) 개수를 세기 위한 변수
-			
-			for(int i=0; i<grass.length(); i++) {
-				
-				// 열린 괄호를 만나면 새로운 공 시작
-				if(grass.charAt(i) == '(') {
-					cnt = 0; // 잡초 개수 초기화
+			int grass = 0;
+			for(int i=0; i<line.length(); i++) {
+				if(line.charAt(i) == '(') {
 					ball++;
+					grass = 0;
 				}
 				
-				else if(grass.charAt(i) == '|') {
-					cnt++;
+				else if(line.charAt(i) == '|') {
+					grass++;
 				}
 				
-				else if(grass.charAt(i) == ')') {
-					// 사이에 잡초가 없으면 추가 처리 없음
-					if(cnt == 0) {
-						continue;
-					} 
-					// 사이에 잡초가 있었다면 공 개수 증가
-					else {
+				else if(line.charAt(i) == ')'){
+					if(grass > 0) {
 						ball++;
-						cnt = 0;
+						grass = 0;
+					} else {
+						continue;
 					}
-				} else {
-					continue;
-				}
+				} 
+				
+				else continue;
 			}
 			
 			System.out.println("#" + t + " " + ball);
