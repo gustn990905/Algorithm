6cb85d057446e1da6b014773f1f4dba9df93fdf8
gustn990905 <julie019019@gmail.com>
2026-07-11 17:23:21 +0900

    [level 1] Title: 삼총사, Time: 0.06 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,13 +1,26 @@
+// 재귀 백트래킹 방식으로 풀어보기
 class Solution {
+
+    public int answer;
+
     public int solution(int[] number) {
-        int answer = 0;
-        for(int i=0; i<number.length-2; i++) {
-        	for(int j=i+1; j<number.length-1; j++) {
-        		for(int k=j+1; k<number.length; k++) {
-        			if(number[i] + number[j] + number[k] == 0) answer++;
-        		}
-        	}
-        }
+        answer = 0;
+        select(number, 0, 0, 0);
         return answer;
+    }
+
+    public void select(int[] number, int start, int depth, int sum) {
+        // 학생 3명을 모두 선택한 경우
+        if(depth == 3) {
+            if(sum == 0) {
+                answer++;
+            }
+            return;
+        }
+
+        // start부터 학생을 한 명씩 선택
+        for(int i=start; i<number.length; i++) {
+            select(number, i+1, depth+1, sum+number[i]);
+        }
     }
 }
