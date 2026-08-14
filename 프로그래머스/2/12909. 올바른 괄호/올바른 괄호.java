faa432d8e70f6fdba07d212a806e38744247b793
gustn990905 <julie019019@gmail.com>
2026-08-14 09:15:26 +0900

    [level 2] Title: 올바른 괄호, Time: 8.70 ms, Memory: 58.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.add('(');
+            }
+
+            else {
+                // 닫는 괄호가 들어왔는데 stack이 비어있으면
+                if(stack.isEmpty()) return false;
+
+                else {
+                    stack.pop();
+                }
+            }
+        }
+        
+        if(stack.isEmpty()) return true;
+        else return false;
+    }
+}
