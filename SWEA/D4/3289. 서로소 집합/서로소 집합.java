e3c1784632d2fbfbf435c6047f57e3104945b3a7
gustn990905 <julie019019@gmail.com>
2026-08-15 14:52:51 +0900

    [D4] Title: 서로소 집합, Time: 447 ms, Memory: 109,496 KB -BaekjoonHub

--- SWEA/D4/3289. 서로소 집합/서로소 집합.java
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -1,51 +1,71 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Solution {
+class Solution {
 
-	static int[] parent;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    static int[] p;
 
-		int T = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		for (int t = 1; t <= T; t++) {
-			int n = sc.nextInt(); // 원소의 개수
-			int m = sc.nextInt(); // 연산의 개수
-			
-			StringBuilder sb = new StringBuilder();
-			
-			parent = new int[n+1]; // 원소가 1번부터 시작
-			for(int p=1; p<=n; p++) {
-				parent[p] = p;
-			}
-			
-			sb.append("#").append(t).append(" ");
-			
-			for(int i=0; i<m; i++) {
-				int c = sc.nextInt();
-				int a = sc.nextInt();
-				int b = sc.nextInt();
-				
-				if(c == 0) {
-					int rootA = find(a);
-					int rootB = find(b);
-					
-					if(rootA != rootB) parent[rootA] = rootB;
-				} else {
-					int rootA = find(a);
-					int rootB = find(b);
-					
-					if(rootA == rootB) sb.append(1);
-					else sb.append(0);
-				}
-			}
-			System.out.println(sb);
-		}
-	}
-	
-	static int find(int v) {
-		if(v != parent[v]) parent[v] = find(parent[v]);
-		return parent[v];
-	}
+        int T = Integer.parseInt(br.readLine());
+
+        for(int t=1; t<=T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            // 부모 배열
+            // 1부터 시작
+            p = new int[n+1];
+
+            // 처음에는 자기 자신이 대표
+            for(int i=1; i<=n; i++) {
+                p[i] = i;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for(int i=0; i<m; i++) {
+                st = new StringTokenizer(br.readLine());
+
+                int command = Integer.parseInt(st.nextToken());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                // 두 집합 합치기
+                if(command == 0) {
+                    union(a, b);
+                }
+
+                // 같은 집합인지 확인
+                else {
+                    if(findSet(a) == findSet(b)) sb.append(1);
+                    else sb.append(0);
+                }
+            }
+            System.out.println("#" + t + " " + sb);
+        }
+    }
+    // 두 집합 합치기
+    static void union(int a, int b) {
+        int pa = findSet(a);
+        int pb = findSet(b);
+
+        if(pa != pb) {
+            p[pb] = pa;
+        }
+    }
+
+    // 두 집합 합치기
+    static int findSet(int x) {
+        if(x != p[x]) {
+            p[x] = findSet(p[x]);
+        }
+
+        return p[x];
+    }
 }
