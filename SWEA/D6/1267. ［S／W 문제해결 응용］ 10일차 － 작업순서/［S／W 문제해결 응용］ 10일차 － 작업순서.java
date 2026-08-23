aeb0a64879c17fbd488dd7b2e8f12b40dbc06fc4
gustn990905 <julie019019@gmail.com>
2026-08-23 09:47:22 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 109 ms, Memory: 28,800 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -1,61 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Solution {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		for(int t=1; t<=10; t++) {
-			
-			StringBuilder sb = new StringBuilder();
-			sb.append("#").append(t).append(" ");
-			
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			int V = Integer.parseInt(st.nextToken()); // 정점의 개수
-			int E = Integer.parseInt(st.nextToken()); // 간선의 개수
-			
-			st = new StringTokenizer(br.readLine());
-			
-			int[][] adjArr = new int[V+1][V+1];
-			int[] inDegree = new int[V+1]; // 진입차수 저장
-			
-			for(int e=0; e<E; e++) {
-				int from = Integer.parseInt(st.nextToken());
-				int to = Integer.parseInt(st.nextToken());
-				
-				adjArr[from][to] = 1;
-				inDegree[to]++;
-			}
-			
-			Queue<Integer> q = new LinkedList<>();
-			
-			for(int e=1; e<=V; e++) {
-				if(inDegree[e] == 0) {
-					q.add(e);
-				}
-			}
-			
-			while(!q.isEmpty()) {
-				int curr = q.poll();
-				sb.append(curr).append(" ");
-				
-				for(int to=1; to<=V; to++) {
-					if(adjArr[curr][to] == 1) {
-						inDegree[to]--;
-						
-						if(inDegree[to] == 0) {
-							q.add(to);
-						}
-					}
-				}
-			}
-			
-			System.out.println(sb);
-		}
-	}
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int t=1; t<=10; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int V = Integer.parseInt(st.nextToken()); // 정점 수
+            int E = Integer.parseInt(st.nextToken()); // 간선 수
+
+            List<Integer>[] adjList = new ArrayList[V+1];
+
+            for(int i=1; i<=V; i++) {
+                adjList[i] = new ArrayList<>();
+            }
+
+            // 진입 차수
+            int[] inDegree = new int[V+1];
+
+            st = new StringTokenizer(br.readLine());
+
+            for(int i=0; i<E; i++) {
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
+
+                adjList[from].add(to);
+
+                inDegree[to]++;
+            }
+
+            Queue<Integer> q = new ArrayDeque<>();
+            
+            // 진입 차수가 0인 작업을 queue에 넣음
+            for(int i=1; i<=V; i++) {
+                if(inDegree[i] == 0) q.add(i);
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            
+            while(!q.isEmpty()) {
+                int curr = q.poll();
+                
+                // 현재 작업을 수행
+                sb.append(curr).append(" ");
+                
+                for(int next : adjList[curr]) {
+                    // curr 작업이 끝났기 때문에 next의 선행 작업 감소
+                    inDegree[next]--;
+                    
+                    // 모든 선행 작업이 끝났다면
+                    if(inDegree[next] == 0) q.add(next);
+                }
+            }
+
+            System.out.println("#" + t + " " + sb);
+        }
+    }
 }
